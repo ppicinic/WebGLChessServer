05160025b47a2b43dd2647a90668a386a5a05a88
Phil Picinic <PhilPicinic@gmail.com>
2013-11-30 03:06:25 -0500

    completed the web server

--- laravel/app/routes.php
+++ laravel/app/routes.php
@@ -13,5 +13,50 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	return View::make('ui');
+});
+
+Route::post('/', function(){
+	$rules = array('moves' => 'required|integer',);
+	$validator = Validator::make(Input::all(), $rules);
+	if($validator->passes()){
+		$full = File::get('../app/full.json');
+		$current = File::get('../app/current.json');
+		$currentJSON = json_decode($current);
+		$fullJSON = json_decode($full);
+		if(!$currentJSON->gameover){
+			$movenumber = $currentJSON->lastmovenumber;
+			$moves = Input::get('moves');
+			$lastmove = $fullJSON->lastmovenumber;
+			for($i = $movenumber; ($i < ($movenumber + $moves)) && ($i < $lastmove); $i++){
+				$currentJSON->moves[$i] = $fullJSON->moves[$i];
+			}
+			$currentJSON->lastmovenumber = count($currentJSON->moves);
+			if($currentJSON->lastmovenumber % 2 == 0){
+				$currentJSON->whitesturn = true;
+			}else{
+				$currentJSON->whitesturn = false;
+			}
+			if($currentJSON->lastmovenumber == $fullJSON->lastmovenumber){
+				$currentJSON->gameover = true;
+			}
+			$newCurrent = json_encode($currentJSON);
+			File::put('../app/current.json', $newCurrent);
+
+		}
+		return Redirect::to('/')->withInput(Input::all());
+	}else{
+		return Redirect::to('/')->withInput(Input::all())->withErrors($validator);
+	}
+});
+
+Route::post('/startgame', function(){
+	$contents = File::get('../app/base.json');
+	File::put('../app/current.json', $contents);
+	return Redirect::to('/');
+});
+
+Route::get('/game', function(){
+	$contents = File::get('../app/current.json');
+	return $contents;
 });
